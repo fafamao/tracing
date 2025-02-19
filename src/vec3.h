5b339388bdc17ae50d8f76d5d3fb809ec0435ab6
Bozhi <baobozhi@outlook.com>
2025-02-19 22:35:03 +0800

    [Tracing] Add Vec3 class

---
+++ src/vec3.h
@@ -0,0 +1,65 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+
+namespace vec3
+{
+    class Vec3
+    {
+    private:
+        double pos[3];
+
+    public:
+        Vec3() {};
+        Vec3(double pos1, double pos2, double pos3) : pos{pos1, pos2, pos3} {};
+        ~Vec3() {};
+
+        double get_x() const
+        {
+            return pos[0];
+        }
+        double get_y() const
+        {
+            return pos[1];
+        }
+        double get_z() const
+        {
+            return pos[2];
+        }
+
+        double operator[](int i) const
+        {
+            return pos[i];
+        }
+        double &operator[](int i) { return pos[i]; }
+
+        Vec3 &operator+=(const Vec3 &vec)
+        {
+            pos[0] += vec.get_x();
+            pos[0] += vec.get_y();
+            pos[0] += vec.get_z();
+            return *this;
+        }
+
+        Vec3 &operator*=(const Vec3 &vec)
+        {
+            pos[0] *= vec.get_x();
+            pos[1] *= vec.get_y();
+            pos[2] *= vec.get_z();
+            return *this;
+        }
+
+        double get_length() const
+        {
+            return std::sqrt(length_squared());
+        }
+
+        double length_squared() const
+        {
+            return pos[0] * pos[0] + pos[1] * pos[1] + pos[2] * pos[2];
+        }
+    };
+}
+
+#endif // VEC3_H
