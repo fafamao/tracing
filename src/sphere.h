f0642f02e19894666d309510374ab72fbabd173c
fafamao <baobozhi@outlook.com>
2025-03-04 16:11:57 +0800

    [Tracing] Fix execution error
    Description:
    - missing mat in sphere class

--- src/sphere.h
+++ src/sphere.h
@@ -38,6 +38,7 @@ public:
         rec.p = r.at(rec.t);
         Vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
